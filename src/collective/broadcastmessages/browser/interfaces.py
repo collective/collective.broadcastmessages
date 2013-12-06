7a31d5522637c71bfe80c99eb5a382bef96f2d4a
Godefroid Chapelle <gotcha@bubblenet.be>
2013-12-06 16:54:02 +0100

    register viewlet - first step

---
+++ src/collective/broadcastmessages/browser/interfaces.py
@@ -0,0 +1,6 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IBroadcastMessagesLayer(IDefaultPloneLayer):
+    """Browser layer used only by collective.broadcastmessages
+    """
