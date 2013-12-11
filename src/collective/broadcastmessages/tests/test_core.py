85baabf0bfe6925cafca90c05598b06f66397afd
Godefroid Chapelle <gotcha@bubblenet.be>
2013-12-11 15:17:35 +0100

    add quicker tests
    rename file

--- src/collective/broadcastmessages/tests/test_core.py
+++ src/collective/broadcastmessages/tests/test_core.py
@@ -1,5 +1,9 @@
 import unittest2 as unittest
 
+from zope.interface import alsoProvides
+
+from plone.app.testing import logout
+
 from Products.CMFCore.utils import getToolByName
 
 from collective.broadcastmessages.testing import \
@@ -23,3 +27,20 @@ class TestExample(unittest.TestCase):
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
+
+    def test_viewlet_shown_to_authenticated(self):
+        view = self.portal.restrictedTraverse('index_html')
+        from collective.broadcastmessages.browser.interfaces import (
+            IBroadcastMessagesLayer,
+            )
+        alsoProvides(view.REQUEST, IBroadcastMessagesLayer)
+        self.assertTrue('broadcast-messages' in view())
+
+    def test_viewlet_not_shown_to_anonymous(self):
+        logout()
+        view = self.portal.restrictedTraverse('index_html')
+        from collective.broadcastmessages.browser.interfaces import (
+            IBroadcastMessagesLayer,
+            )
+        alsoProvides(view.REQUEST, IBroadcastMessagesLayer)
+        self.assertFalse('broadcast-messages' in view())
