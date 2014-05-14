178dcb5efef3eaecaffa607b447263b5f634fabb
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-14 15:52:49 +0200

    savepoint : get messages from file system, first step
    not in working state

--- src/collective/broadcastmessages/messages.py
+++ src/collective/broadcastmessages/messages.py
@@ -1,11 +1,25 @@
 import transaction
+import os.path
 
+from App.config import getConfiguration
 from zope.component import getSiteManager
 
 from collective.broadcastmessages.interfaces import IBroadcastMessages
 
 
+DIRECTORYNAME = 'broadcastmessages'
+
+
 def initializeFromFileSystem(event):
+    cfg = getConfiguration()
+    messages_filename = os.path.join(cfg.clienthome, MESSAGES_FILENAME)
+    if not os.path.exists(messages_filename):
+        return
+    file = open(messages_filename)
+    try:
+        all_messages = parseMessageFile(file)
+    finally:
+        file.close()
     db = event.database
     conn = db.open()
     try:
@@ -13,7 +27,7 @@ def initializeFromFileSystem(event):
         app = root['Application']
         try:
             for portal in app.objectValues(spec="Plone Site"):
-                registerBroadcastMessages(portal)
+                registerBroadcastMessages(portal, messages)
             transaction.commit()
         except:
             transaction.abort()
@@ -22,9 +36,20 @@ def initializeFromFileSystem(event):
         conn.close()
 
 
-def registerBroadcastMessages(portal):
+def parseMessageFile(file):
+    import ConfigParser, os
+
+    config = ConfigParser.SafeConfigParser()
+    config.readfp(file)
+    result = dict()
+    for section in config.sections():
+        result[section] = config.options(section)
+    return result
+
+
+def registerBroadcastMessages(portal, messages):
     sm = getSiteManager(portal)
     sm.registerUtility(
-        component=['maintenance', 'tuesday'],
+        component=messages,
         provided=IBroadcastMessages,
         )
